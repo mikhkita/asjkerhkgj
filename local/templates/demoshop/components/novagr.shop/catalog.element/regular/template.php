17ae6fb05c6297fba57b3d3db6bb2c8cca2d1c73
mulenokd <mulenokd@gmail.com>
2018-07-15 16:21:28 +0700

    123123

--- local/templates/demoshop/components/novagr.shop/catalog.element/regular/template.php
+++ local/templates/demoshop/components/novagr.shop/catalog.element/regular/template.php
@@ -233,7 +233,7 @@ if (!empty($arResult['ELEMENT']["ID"])) {
                         ),
                         "img"=>array(
                             "src"=>(!empty($arResult['PREVIEW_PICTURE'][$color])) ? $arResult['PREVIEW_PICTURE'][$color] : "/local/templates/demoshop/images/not-f.jpg",
-                            "width"=>"44",
+                            "width"=>"78",
                             "height"=>"42",
                             "position"=>"absolute",
                             "top"=> "0",
@@ -243,6 +243,18 @@ if (!empty($arResult['ELEMENT']["ID"])) {
                         )
                     );
                 }
+                
+                $i=0;
+            	foreach ($arColors['COLORS'] as $key => $value) {
+            		if ($i > 0) {
+            			$params[$i-1]["img"]["src"] = $arResult['DETAIL_IMAGES'][$key]["curPhotosSmall"][0];
+            		}
+            		else{
+            			$j = count($params) - 1;
+            			$params[$j]["img"]["src"] = $arResult['DETAIL_IMAGES'][$key]["curPhotosSmall"][0];
+            		}
+            		$i++;
+            	}
                 Novagroup_Classes_General_Main::getView('catalog.element','colors_button', array("params"=>$params) );
             }
 
